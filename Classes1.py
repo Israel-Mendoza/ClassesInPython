f618726343f682aae328b5de4efb4e1756dbea4f
Israel Mendoza <xander_irsl@hotmail.com>
2020-07-19 22:46:18 -0500

    Initial commit containing basic Classes metadata

---
+++ Classes1.py
@@ -0,0 +1,7 @@
+class MyClass:
+    pass
+
+
+print(f"Name of MyClass: {MyClass.__name__}")
+print(f"Type of MyClass: {type(MyClass)}")
+print(f"Is MyClass an instance of 'type'? {isinstance(MyClass, type)}")
